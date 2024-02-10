41b189a72e619203dfab5059c850131e2fc51dbf
Halanau327 <a.halanau@gmail.com>
2024-02-10 15:18:29 +0300

    first commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,18 @@
+const Profile = () => {
+    return <div className='content'>
+        <div>
+            <img src='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg'></img>
+        </div>
+        <div>
+            ava _ description
+        </div>
+        <div>
+            My posts
+            <div>New post</div>
+            <div>Post 1</div>
+            <div>Post 2</div>
+        </div>
+    </div>
+}
+
+export default Profile;
